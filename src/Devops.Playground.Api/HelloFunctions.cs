2361dee899a64c27ded4dba1a419549265aed8e3
Thorsten Sauter <Thorsten.Sauter@gmail.com>
2021-09-14 20:01:24 +0200

    - Cleaned up project files - Updated dependencies - Added placeholder unit test project - Transformed function to not be static - Renamed Function1 to HelloFunctions - Made HelloFunctions accessible anonymously - Adjusted route prefix

--- src/Devops.Playground.Api/HelloFunctions.cs
+++ src/Devops.Playground.Api/HelloFunctions.cs
@@ -1,4 +1,3 @@
-using System.Collections.Generic;
 using System.Net;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Http;
@@ -6,20 +5,22 @@ using Microsoft.Extensions.Logging;
 
 namespace Devops.Playground.Api
 {
-    public static class Function1
+    public class HelloFunctions
     {
-        [Function("Function1")]
-        public static HttpResponseData Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequestData req,
+        private readonly ILogger<HelloFunctions> _logger;
+
+        public HelloFunctions(ILogger<HelloFunctions> logger) =>
+            _logger = logger;
+
+
+        [Function("HelloFunctions")]
+        public HttpResponseData Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestData req,
             FunctionContext executionContext)
         {
-            var logger = executionContext.GetLogger("Function1");
-            logger.LogInformation("C# HTTP trigger function processed a request.");
-
+            _logger.LogInformation("C# HTTP trigger function processed a request");
             var response = req.CreateResponse(HttpStatusCode.OK);
             response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
-
             response.WriteString("Welcome to Azure Functions!");
-
             return response;
         }
     }
