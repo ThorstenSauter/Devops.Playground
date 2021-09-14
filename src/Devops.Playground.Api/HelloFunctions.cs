c9fb4fcea0f1bc7e983152f7cb326e75260f010c
Thorsten Sauter <Thorsten.Sauter@gmail.com>
2021-09-14 22:03:49 +0200

    Update to .NET 6 RC1 - Updated TargetFramework - Moved to file-scoped namespaces - Adjusted build pipeline to acquire .NET 6 SDK and switch to Azure Functions v4

--- src/Devops.Playground.Api/HelloFunctions.cs
+++ src/Devops.Playground.Api/HelloFunctions.cs
@@ -3,25 +3,23 @@ using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Http;
 using Microsoft.Extensions.Logging;
 
-namespace Devops.Playground.Api
+namespace Devops.Playground.Api;
+
+public class HelloFunctions
 {
-    public class HelloFunctions
+    private readonly ILogger<HelloFunctions> _logger;
+
+    public HelloFunctions(ILogger<HelloFunctions> logger) =>
+        _logger = logger;
+
+    [Function("HelloFunctions")]
+    public HttpResponseData Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestData req,
+        FunctionContext executionContext)
     {
-        private readonly ILogger<HelloFunctions> _logger;
-
-        public HelloFunctions(ILogger<HelloFunctions> logger) =>
-            _logger = logger;
-
-
-        [Function("HelloFunctions")]
-        public HttpResponseData Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestData req,
-            FunctionContext executionContext)
-        {
-            _logger.LogInformation("C# HTTP trigger function processed a request");
-            var response = req.CreateResponse(HttpStatusCode.OK);
-            response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
-            response.WriteString("Welcome to Azure Functions!");
-            return response;
-        }
+        _logger.LogInformation("C# HTTP trigger function processed a request");
+        var response = req.CreateResponse(HttpStatusCode.OK);
+        response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
+        response.WriteString("Welcome to Azure Functions!");
+        return response;
     }
 }
