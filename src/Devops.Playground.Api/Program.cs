317d0b4f925f18240173775c90cd3edee70f007e
Thorsten Sauter <Thorsten.Sauter@gmail.com>
2021-09-14 23:33:56 +0200

    Switched to running the Host asynchronously

--- src/Devops.Playground.Api/Program.cs
+++ src/Devops.Playground.Api/Program.cs
@@ -1,15 +1,16 @@
+using System.Threading.Tasks;
 using Microsoft.Extensions.Hosting;
 
 namespace Devops.Playground.Api;
 
 public class Program
 {
-    public static void Main()
+    public static async Task Main()
     {
         var host = new HostBuilder()
             .ConfigureFunctionsWorkerDefaults()
             .Build();
 
-        host.Run();
+        await host.RunAsync();
     }
 }
