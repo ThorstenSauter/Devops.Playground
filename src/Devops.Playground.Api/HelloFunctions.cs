1fb397d8f162a60c5962de90e17a63e461255aef
Thorsten Sauter <Thorsten.Sauter@gmail.com>
2021-09-15 09:31:43 +0200

    Reverted back to .NET 5 and Linux hosted function (#7)

--- src/Devops.Playground.Api/HelloFunctions.cs
+++ src/Devops.Playground.Api/HelloFunctions.cs
@@ -3,23 +3,24 @@ using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Http;
 using Microsoft.Extensions.Logging;
 
-namespace Devops.Playground.Api;
-
-public class HelloFunctions
+namespace Devops.Playground.Api
 {
-    private readonly ILogger<HelloFunctions> _logger;
-
-    public HelloFunctions(ILogger<HelloFunctions> logger) =>
-        _logger = logger;
-
-    [Function("HelloFunctions")]
-    public HttpResponseData Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestData req,
-        FunctionContext executionContext)
+    public class HelloFunctions
     {
-        _logger.LogInformation("C# HTTP trigger function processed a request");
-        var response = req.CreateResponse(HttpStatusCode.OK);
-        response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
-        response.WriteString("Welcome to Azure Functions!");
-        return response;
+        private readonly ILogger<HelloFunctions> _logger;
+
+        public HelloFunctions(ILogger<HelloFunctions> logger) =>
+            _logger = logger;
+
+        [Function("HelloFunctions")]
+        public HttpResponseData Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestData req,
+            FunctionContext executionContext)
+        {
+            _logger.LogInformation("C# HTTP trigger function processed a request");
+            var response = req.CreateResponse(HttpStatusCode.OK);
+            response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
+            response.WriteString("Welcome to Azure Functions!");
+            return response;
+        }
     }
 }
--- src/Devops.Playground.Api/Program.cs
+++ src/Devops.Playground.Api/Program.cs
@@ -1,16 +1,17 @@
 using System.Threading.Tasks;
 using Microsoft.Extensions.Hosting;
 
-namespace Devops.Playground.Api;
-
-public class Program
+namespace Devops.Playground.Api
 {
-    public static async Task Main()
+    public class Program
     {
-        var host = new HostBuilder()
-            .ConfigureFunctionsWorkerDefaults()
-            .Build();
+        public static async Task Main()
+        {
+            var host = new HostBuilder()
+                .ConfigureFunctionsWorkerDefaults()
+                .Build();
 
-        await host.RunAsync();
+            await host.RunAsync();
+        }
     }
 }
