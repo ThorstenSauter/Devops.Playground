8604c2a024af0cedc045382ffb33a4f5afeab93a
Thorsten Sauter <Thorsten.Sauter@gmail.com>
2021-09-29 19:29:43 +0200

    Retargeted demo function to .NET 6 (#9)

--- src/Devops.Playground.Api/Program.cs
+++ src/Devops.Playground.Api/Program.cs
@@ -1,17 +1,7 @@
-using System.Threading.Tasks;
 using Microsoft.Extensions.Hosting;
 
-namespace Devops.Playground.Api
-{
-    public class Program
-    {
-        public static async Task Main()
-        {
-            var host = new HostBuilder()
-                .ConfigureFunctionsWorkerDefaults()
-                .Build();
+var host = new HostBuilder()
+    .ConfigureFunctionsWorkerDefaults()
+    .Build();
 
-            await host.RunAsync();
-        }
-    }
-}
+await host.RunAsync();
